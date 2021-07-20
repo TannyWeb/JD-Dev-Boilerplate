3ac06b8f5b4f96ed97cce376660757f0257a7bc2
Tanvir Ali <Tanvir.Ali@jdplc.com>
2021-07-20 10:25:19 +0100

    added body loaded el

--- src/V1/v1.js
+++ src/V1/v1.js
@@ -3,3 +3,19 @@
 import './v1.scss';
 
 console.log('hello')
+
+waitFor('body', initT01)
+
+function initT01() {
+    if (document.body.className.indexOf('test01_loaded') === -1) {
+        t01Changes();
+    } else {
+        console.warn('Experiment not loaded');
+    }
+}
+
+function t01Changes() {
+    document.body.classList.add('test01_loaded');
+
+    // add your changes here
+}
